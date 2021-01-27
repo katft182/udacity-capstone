a18c24a7470c85293aeb8e87a5a3b0429cfba674
Katy Faulkner <katft182@gmail.com>
2021-01-27 16:43:14 +0000

    Update to version 3 to check rolling update

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World version2!'
+    return 'Hello, World version 3!'
     
 @app.route('/goodbye')
 def goodbye_world():
-    return 'Goodbye, World version2!'    
+    return 'Goodbye, World version 3!'    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
